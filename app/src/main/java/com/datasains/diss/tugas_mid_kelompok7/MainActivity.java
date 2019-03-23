c8209fbe91786adccbb9715738a453d34f8e1119
sulaiman13 <sulaiman.tsany@gmail.com>
2019-03-23 16:54:53 +0800

    test realm

--- app/src/main/java/com/datasains/diss/tugas_mid_kelompok7/MainActivity.java
+++ app/src/main/java/com/datasains/diss/tugas_mid_kelompok7/MainActivity.java
@@ -4,24 +4,43 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
-
+    Realm realm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Realm.init(this);
+
+        realm = Realm.getDefaultInstance();
+
     }
 
     public void loginButton(View view) {
 //        Intent intent = new Intent(this, TugasDialogActivity.class);
 //        startActivity(intent);
 //        this.finish();
+
+        String phone = ((TextView)findViewById(R.id.phone)).getText().toString();
+        String password = ((TextView)findViewById(R.id.password)).getText().toString();
+
+        UserModel user = realm.where(UserModel.class).equalTo("id", phone).findFirst();
+        Toast.makeText(this, user.getName(),Toast.LENGTH_LONG).show();
+
+//        realm.beginTransaction();
+//        user.deleteAllFromRealm();
+//        realm.commitTransaction();
     }
 
-    public void registerButton(View view) {
+    public void registerPage(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
-        this.finish();
+        //this.finish();
     }
 }
--- app/src/main/java/com/datasains/diss/tugas_mid_kelompok7/RegisterActivity.java
+++ app/src/main/java/com/datasains/diss/tugas_mid_kelompok7/RegisterActivity.java
@@ -2,12 +2,41 @@ package com.datasains.diss.tugas_mid_kelompok7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class RegisterActivity extends AppCompatActivity {
+    Realm realm;
+
+    RealmResults<UserModel> user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        realm = Realm.getDefaultInstance();
+    }
+
+    public void register(View view) {
+        String phone = ((TextView)findViewById(R.id.phone)).getText().toString();
+        String name = ((TextView)findViewById(R.id.name)).getText().toString();
+        String password = ((TextView)findViewById(R.id.password)).getText().toString();
+        UserModel model = new UserModel(phone, name, password);
+        Toast.makeText(this, "berhasil"+phone+name+password, Toast.LENGTH_LONG).show();
+
+        realm.beginTransaction();
+        realm.copyToRealm(model);
+        realm.commitTransaction();
+    }
+
+    public void cek(View view) {
+        user = realm.where(UserModel.class).findAll();
+        String size = String.valueOf(user.size());
+        Toast.makeText(this, size, Toast.LENGTH_LONG).show();
     }
 }
